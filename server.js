94f8539e7327604f95adb95ed00a2fe664085584
gioeleminardi <gioelem3@gmail.com>
2017-11-29 17:09:55 +0100

    added .env file

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
-var app = require('./app/app');
-var port = process.env.PORT || 3000;
+const dotenv = require('dotenv');
+dotenv.load();
+const app = require('./app/app');
+const port = process.env.PORT || 3000;
 
 const server = app.listen(port, () => {
     console.log(`Listening on port ${port}`)
